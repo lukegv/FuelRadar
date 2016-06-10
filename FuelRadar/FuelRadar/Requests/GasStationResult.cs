56ec4bddf83aa9d158225aba83ced4336f132926
Lukas Körfer <lu.koerfer@googlemail.com>
2016-06-10 03:25:15 +0200

    Added request results

---
+++ FuelRadar/FuelRadar/Requests/GasStationResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace FuelRadar.Model
+{
+    public class GasStationResult
+    {
+        [JsonProperty("id")]
+        public String ID { get; set; }
+
+        [JsonProperty("name")]
+        public String Name { get; set; }
+        [JsonProperty("brand")]
+        public String Brand { get; set; }
+
+        [JsonProperty("lat")]
+        public double Latitude { get; set; }
+        [JsonProperty("lng")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("diesel")]
+        public double DieselPrice { get; set; }
+        [JsonProperty("e5")]
+        public double E5Price { get; set; }
+        [JsonProperty("e10")]
+        public double E10Price { get; set; }
+    }
+}
---
+++ FuelRadar/FuelRadar/Requests/PriceResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace FuelRadar.Requests
+{
+    public class PriceResult
+    {
+        [JsonProperty("status")]
+        public String Status { get; set; }
+
+        [JsonProperty("diesel")]
+        public double DieselPrice { get; set; }
+        [JsonProperty("e5")]
+        public double E5Price { get; set; }
+        [JsonProperty("e10")]
+        public double E10Price { get; set; }
+    }
+}
